d406e4addddd5512f9230c387466da388182f5fd
kanmpatel <100915585+kanmpatel@users.noreply.github.com>
2023-07-31 20:12:04 +0100

    Megha Jadvani (#1)
    * This my chages for megha
    
    * remove unneccsery code
    
    * grdoi
    
    * relove conflints
    
    ---------
    
    Co-authored-by: MeghaJadvani <meghadj1803@gmail.com>

--- src/main/java/Bansari.java
+++ src/main/java/Bansari.java
@@ -1,5 +1,5 @@
 public class Bansari {
     public static void main(String[] args) {
-        System.out.println("My Name Is Bansari RikitaChanges");
+        System.out.println("My Name Is Bansari megha RikitaChanges");
     }
 }
